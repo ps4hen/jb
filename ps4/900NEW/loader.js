d459274b9efb4b36c9f144c11ef328859484b3c3
ps4hen <119159133+ps4hen@users.noreply.github.com>
2025-10-24 09:39:15 +0500

    Delete ps4/900NEW directory

--- ps4/900NEW/loader.js
+++
@@ -1,7 +0,0 @@
-function StartTimer(){StartTime=Date.now();}
-function EndTimer(){
-    EndTime=Date.now()-StartTime;
-    hrs=Math.floor(EndTime/1000/60/60);
-    min=Math.floor(EndTime/1000/60-hrs*60)
-    sec=Math.floor(EndTime/1000-min*60)
-    showMessage("<div style='color:green'>GoldHEN загружен за "+sec+" сек!</div> нажмите кнопку PS для выхода"); }
