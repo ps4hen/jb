8b8165a4fc53c1e4ebb9561d8d0e2b0f724dc205
ps4hen <119159133+ps4hen@users.noreply.github.com>
2025-11-10 09:03:16 +0500

    ps5 hen

---
+++ ps5/payload_map.js
@@ -0,0 +1,92 @@
+const payload_map = [
+
+    {
+        displayTitle: 'etaHEN',
+        description: '',
+        info: 'etaHEN 2.2b by LM',
+        fileName: 'etaHEN-22B.bin.gz',
+        author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+        version: '2.2 beta'
+    },
+{
+        displayTitle: 'etaHEN 1.8b',
+        description: '',
+        info: 'etaHEN 1.8b by LM',
+        fileName: 'etaHEN-1.8B.bin.gz',
+        author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+        version: '1.8 beta'
+    },
+    {
+        displayTitle: 'HW Info',
+        description: '',
+        info: 'Temperature information, frequency',
+        fileName: 'hwinfo-tornblom.elf',
+        author: '?',
+        source: '?',
+        version: '?'
+    },
+    {
+        displayTitle: 'ELF Loader',
+        description: '',
+        info: 'Uses port 9021. Persistent network elf loader',
+        fileName: 'elfldr.elf',
+        author: 'john-tornblom',
+        source:'https://github.com/ps5-payload-dev/elfldr/releases',
+        version: '0.14'
+    },
+    {
+        displayTitle: 'Web SRV',
+        description: '',
+        info: 'Uses elfldr. Custom homebrew loader Runs on port 8000',
+        fileName: 'websrv.elf',
+        author: 'john-tornblom',
+        loader: 'john-tornblom-elfldr',
+        source:'https://github.com/ps5-payload-dev/websrv/releases',
+        version: '0.4'
+    },
+    {
+        displayTitle: 'Versions',
+        description: '',
+        info: 'Shows kernel build, os and sdk versions',
+        fileName: 'versions.elf',
+        author: 'SiSTRo',
+        source:'https://github.com/SiSTR0/ps5-versions/releases/download/v1.0/ps5-versions.elf',
+        version: '1.0'
+    },
+    {
+        displayTitle: 'Cache Remove',
+        description: '',
+        info: 'Removes appcache from browser',
+        fileName: 'Browser_appCache_remove.elf',
+        author: 'Storm21CH',
+        source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
+        version: '1.0fix'
+    },
+    {
+        displayTitle: 'Fan control',
+        description: '',
+        info: 'Fan threshold set 60°',
+        fileName: 'fan_threshold.elf',
+        author: '?',
+        source: '?',
+        version: '1.0'
+    },
+  // {
+    //     displayTitle: 'ps5-welcome',
+    //     description: '',
+    //     info: 'ps5-welcome',
+    //     fileName: 'ps5-welcome.elf',
+    //     author: 'mour0ne',
+    //     version: '?'
+    // },
+      // {
+    //     displayTitle: 'ps5debug_10b2',
+    //     description: '',
+    //     info: 'ps5debug_10b2',
+    //     fileName: 'ps5debug_10b2.elf',
+    //     author: 'goldhen',
+    //     version: '?'
+    // },
+];
+
+
